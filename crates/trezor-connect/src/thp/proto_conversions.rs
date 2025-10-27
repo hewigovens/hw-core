73275103e01216846a609523c1331bb649c5045b
hewigovens <360470+hewigovens@users.noreply.github.com>
2025-10-27 14:35:49 +0900

    feat(thp): add protobuf mapping helpers

--- crates/trezor-connect/src/thp/proto_conversions.rs
+++ crates/trezor-connect/src/thp/proto_conversions.rs
@@ -6,7 +6,7 @@ use thiserror::Error;
 
 use super::proto;
 use super::types::{
-    CredentialRequest, CredentialResponse, CreateSessionRequest, PairingMethod, PairingRequest,
+    CredentialRequest, CredentialResponse, PairingMethod, PairingRequest,
     PairingRequestApproved, PairingTagResponse, SelectMethodRequest, SelectMethodResponse,
     ThpProperties,
 };
@@ -30,7 +30,10 @@ pub struct EncodedMessage {
     pub payload: Vec<u8>,
 }
 
-fn encode_message<M: Message>(message_type: proto::ThpMessageType, message: &M) -> Result<EncodedMessage, ProtoMappingError> {
+fn encode_message<M: Message>(
+    message_type: proto::ThpMessageType,
+    message: &M,
+) -> Result<EncodedMessage, ProtoMappingError> {
     let mut payload = Vec::new();
     message.encode(&mut payload)?;
     Ok(EncodedMessage {
@@ -39,7 +42,9 @@ fn encode_message<M: Message>(message_type: proto::ThpMessageType, message: &M) 
     })
 }
 
-pub fn encode_pairing_request(request: &PairingRequest) -> Result<EncodedMessage, ProtoMappingError> {
+pub fn encode_pairing_request(
+    request: &PairingRequest,
+) -> Result<EncodedMessage, ProtoMappingError> {
     let message = proto::ThpPairingRequest {
         host_name: request.host_name.clone(),
         app_name: request.app_name.clone(),
@@ -47,12 +52,16 @@ pub fn encode_pairing_request(request: &PairingRequest) -> Result<EncodedMessage
     encode_message(proto::ThpMessageType::ThpPairingRequest, &message)
 }
 
-pub fn decode_pairing_request_approved(payload: &[u8]) -> Result<PairingRequestApproved, ProtoMappingError> {
+pub fn decode_pairing_request_approved(
+    payload: &[u8],
+) -> Result<PairingRequestApproved, ProtoMappingError> {
     let _ = proto::ThpPairingRequestApproved::decode(payload)?;
     Ok(PairingRequestApproved)
 }
 
-pub fn encode_select_method(request: &SelectMethodRequest) -> Result<EncodedMessage, ProtoMappingError> {
+pub fn encode_select_method(
+    request: &SelectMethodRequest,
+) -> Result<EncodedMessage, ProtoMappingError> {
     let message = proto::ThpSelectMethod {
         selected_pairing_method: pairing_method_to_proto(request.method),
     };
@@ -71,27 +80,29 @@ pub fn decode_select_method_response(
         proto::ThpMessageType::ThpCodeEntryCommitment => {
             let msg = proto::ThpCodeEntryCommitment::decode(payload)?;
             Ok(SelectMethodResponse::CodeEntryCommitment {
-                commitment: msg.commitment.unwrap_or_default(),
+                commitment: msg.commitment,
             })
         }
         proto::ThpMessageType::ThpPairingPreparationsFinished => {
             let _ = proto::ThpPairingPreparationsFinished::decode(payload)?;
             Ok(SelectMethodResponse::PairingPreparationsFinished { nfc_data: None })
         }
-        _ => Err(ProtoMappingError::UnexpectedMessage(message_type as i32 as u16)),
+        _ => Err(ProtoMappingError::UnexpectedMessage(
+            message_type as i32 as u16,
+        )),
     }
 }
 
 pub fn encode_qr_tag(tag: &str) -> Result<EncodedMessage, ProtoMappingError> {
     let message = proto::ThpQrCodeTag {
-        tag: tag.to_string(),
+        tag: tag.as_bytes().to_vec(),
     };
     encode_message(proto::ThpMessageType::ThpQrCodeTag, &message)
 }
 
 pub fn encode_nfc_tag(tag: &str) -> Result<EncodedMessage, ProtoMappingError> {
     let message = proto::ThpNfcTagHost {
-        tag: tag.to_string(),
+        tag: tag.as_bytes().to_vec(),
     };
     encode_message(proto::ThpMessageType::ThpNfcTagHost, &message)
 }
@@ -101,8 +112,8 @@ pub fn encode_code_entry_tag(
     tag: &[u8],
 ) -> Result<EncodedMessage, ProtoMappingError> {
     let message = proto::ThpCodeEntryCpaceHostTag {
-        cpace_host_public_key: Some(cpace_host_public_key.to_vec()),
-        tag: Some(tag.to_vec()),
+        cpace_host_public_key: cpace_host_public_key.to_vec(),
+        tag: tag.to_vec(),
     };
     encode_message(proto::ThpMessageType::ThpCodeEntryCpaceHostTag, &message)
 }
@@ -118,23 +129,19 @@ pub fn decode_tag_response(
     match message_type {
         proto::ThpMessageType::ThpQrCodeSecret => {
             let msg = proto::ThpQrCodeSecret::decode(payload)?;
-            Ok(ParsedTagResponse {
-                secret: msg.secret.unwrap_or_default(),
-            })
+            Ok(ParsedTagResponse { secret: msg.secret })
         }
         proto::ThpMessageType::ThpNfcTagTrezor => {
             let msg = proto::ThpNfcTagTrezor::decode(payload)?;
-            Ok(ParsedTagResponse {
-                secret: msg.tag.unwrap_or_default(),
-            })
+            Ok(ParsedTagResponse { secret: msg.tag })
         }
         proto::ThpMessageType::ThpCodeEntrySecret => {
             let msg = proto::ThpCodeEntrySecret::decode(payload)?;
-            Ok(ParsedTagResponse {
-                secret: msg.secret.unwrap_or_default(),
-            })
+            Ok(ParsedTagResponse { secret: msg.secret })
         }
-        _ => Err(ProtoMappingError::UnexpectedMessage(message_type as i32 as u16)),
+        _ => Err(ProtoMappingError::UnexpectedMessage(
+            message_type as i32 as u16,
+        )),
     }
 }
 
@@ -156,12 +163,12 @@ fn pairing_method_to_proto(method: PairingMethod) -> i32 {
 pub fn proto_to_pairing_methods(values: &[i32]) -> Result<Vec<PairingMethod>, ProtoMappingError> {
     values
         .iter()
-        .map(|v| match proto::ThpPairingMethod::from_i32(*v) {
-            Some(proto::ThpPairingMethod::SkipPairing) => Ok(PairingMethod::SkipPairing),
-            Some(proto::ThpPairingMethod::CodeEntry) => Ok(PairingMethod::CodeEntry),
-            Some(proto::ThpPairingMethod::QrCode) => Ok(PairingMethod::QrCode),
-            Some(proto::ThpPairingMethod::Nfc) => Ok(PairingMethod::Nfc),
-            None => Err(ProtoMappingError::InvalidEnum(*v)),
+        .map(|v| match proto::ThpPairingMethod::try_from(*v) {
+            Ok(proto::ThpPairingMethod::SkipPairing) => Ok(PairingMethod::SkipPairing),
+            Ok(proto::ThpPairingMethod::CodeEntry) => Ok(PairingMethod::CodeEntry),
+            Ok(proto::ThpPairingMethod::QrCode) => Ok(PairingMethod::QrCode),
+            Ok(proto::ThpPairingMethod::Nfc) => Ok(PairingMethod::Nfc),
+            Err(_) => Err(ProtoMappingError::InvalidEnum(*v)),
         })
         .collect()
 }
@@ -169,34 +176,27 @@ pub fn proto_to_pairing_methods(values: &[i32]) -> Result<Vec<PairingMethod>, Pr
 pub fn encode_credential_request(
     request: &CredentialRequest,
 ) -> Result<EncodedMessage, ProtoMappingError> {
+    let credential_bytes = request
+        .credential
+        .as_ref()
+        .map(|cred| Vec::from_hex(cred))
+        .transpose()?;
+
     let message = proto::ThpCredentialRequest {
-        host_static_public_key: Some(request.host_static_public_key.clone()),
+        host_static_public_key: request.host_static_public_key.clone(),
         autoconnect: Some(request.autoconnect),
-        credential: request
-            .credential
-            .as_ref()
-            .map(|cred| Vec::from_hex(cred))
-            .transpose()?
-            .map(Into::into),
+        credential: credential_bytes,
     };
     encode_message(proto::ThpMessageType::ThpCredentialRequest, &message)
 }
 
-pub fn decode_credential_response(
-    payload: &[u8],
-) -> Result<CredentialResponse, ProtoMappingError> {
+pub fn decode_credential_response(payload: &[u8]) -> Result<CredentialResponse, ProtoMappingError> {
     let msg = proto::ThpCredentialResponse::decode(payload)?;
-    let credential_bytes = msg
-        .credential
-        .unwrap_or_default()
-        .into_bytes();
+    let credential_hex = msg.credential.encode_hex::<String>();
 
     Ok(CredentialResponse {
-        trezor_static_public_key: msg
-            .trezor_static_public_key
-            .unwrap_or_default()
-            .into_bytes(),
-        credential: credential_bytes.encode_hex::<String>(),
+        trezor_static_public_key: msg.trezor_static_public_key,
+        credential: credential_hex,
         autoconnect: false,
     })
 }
@@ -205,23 +205,10 @@ pub fn encode_end_request() -> Result<EncodedMessage, ProtoMappingError> {
     encode_message(proto::ThpMessageType::ThpEndRequest, &proto::ThpEndRequest {})
 }
 
-pub fn encode_create_session_request(
-    request: &CreateSessionRequest,
-) -> Result<EncodedMessage, ProtoMappingError> {
-    let message = proto::ThpCreateNewSession {
-        passphrase: request.passphrase.clone(),
-        on_device: Some(request.on_device),
-        derive_cardano: Some(request.derive_cardano),
-    };
-    encode_message(proto::ThpMessageType::ThpCreateNewSession, &message)
-}
-
-pub fn decode_device_properties(
-    payload: &[u8],
-) -> Result<ThpDeviceProperties, ProtoMappingError> {
+pub fn decode_device_properties(payload: &[u8]) -> Result<ThpProperties, ProtoMappingError> {
     let msg = proto::ThpDeviceProperties::decode(payload)?;
     let pairing_methods = proto_to_pairing_methods(&msg.pairing_methods)?;
-    Ok(ThpDeviceProperties {
+    Ok(ThpProperties {
         internal_model: msg.internal_model,
         model_variant: msg.model_variant.unwrap_or(0),
         protocol_version_major: msg.protocol_version_major,
@@ -229,4 +216,3 @@ pub fn decode_device_properties(
         pairing_methods,
     })
 }
-*** End Patch
